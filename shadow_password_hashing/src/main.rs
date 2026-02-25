4a0f63f655933ac462a3b82e6abbfa430047eeab
VelesW <michalwilk296@gmail.com>
2026-02-25 20:07:29 +0100

    Password cracking initial version

---
+++ shadow_password_hashing/src/main.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    shadow: Option<String>,
+
+    #[clap(short, long, value_parser)]
+    wordlist: Option<String>,
+}
+
+const WORDLIST: &str = "wordlist";
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let shadow = if let Some(shadow) = args.shadow {
+        shadow
+    } else {
+        "shadow".to_string()
+    };
+
+    let wordlist = if let Some(wordlist) = args.wordlist {
+        wordlist
+    } else {
+        WORDLIST.to_string()
+    };
+
+    let list = lib::crack_shadow(&shadow, &wordlist)?;
+
+    for (user, password) in list {
+        println!("Extracted {} : {}", user, password);
+    }
+
+    Ok(())
+}
